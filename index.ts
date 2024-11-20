467e55393b8068e222c410177eac135c7f24f152
gideonadeti <gideonadeti0@gmail.com>
2024-11-20 17:09:18 +0000

    Correct error in logger configuration

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const logger = debug("app:server");
+const logger = debug("personal-library-backend:server");
 const port = normalizePort(process.env.PORT || "3000");
 
 app.set("port", port);
